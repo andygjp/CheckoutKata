120891579c94cfbeb4da5d5f9c26b2d66d13170c
Andrew Pattison <andygjp@hotmail.com>
2015-11-18 15:59:03 +0000

    Making a start on the first case.

---
+++ CheckoutKata/CheckoutKata.Tests/Tests.cs
@@ -0,0 +1,24 @@
+﻿namespace CheckoutKata.Tests
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public class When_no_items_are_scanned
+    {
+        [Fact]
+        public void It_should_return_zero()
+        {
+            var sut = new Checkout();
+            double actual = sut.GetTotal();
+            actual.Should().Be(0);
+        }
+    }
+
+    public class Checkout
+    {
+        public double GetTotal()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
