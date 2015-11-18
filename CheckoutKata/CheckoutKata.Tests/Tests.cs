cc4b9adef73dab07f09d09570a52b1f36a900ea7
Andrew Pattison <andygjp@hotmail.com>
2015-11-18 16:05:57 +0000

    Added another case.

--- CheckoutKata/CheckoutKata.Tests/Tests.cs
+++ CheckoutKata/CheckoutKata.Tests/Tests.cs
@@ -14,11 +14,40 @@
         }
     }
 
+    public class When_single_item_is_scanned
+    {
+        [Fact]
+        public void It_should_return_expected_value()
+        {
+            var sut = new Checkout();
+            sut.Scan(new Item('A', 50.0));
+            double actual = sut.GetTotal();
+            actual.Should().Be(0);
+        }
+    }
+
+    public class Item
+    {
+        public Item(char name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public char Name { get; }
+        public double Price { get; }
+    }
+
     public class Checkout
     {
         public double GetTotal()
         {
             return 0;
+        }
+
+        public void Scan(Item item)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
