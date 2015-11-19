06623940ce707cdb94ab5ff984117812f59bc234
Andrew Pattison <andygjp@hotmail.com>
2015-11-19 17:22:18 +0000

    Added new case.

--- CheckoutKata/CheckoutKata.Tests/Tests.cs
+++ CheckoutKata/CheckoutKata.Tests/Tests.cs
@@ -14,11 +14,28 @@
         }
     }
 
+    public class When_I_scan_in_item_A
+    {
+        [Fact]
+        public void It_should_return_fifty()
+        {
+            var sut = new Checkout();
+            sut.Scan('A');
+            double actual = sut.GetTotal();
+            actual.Should().Be(50);
+        }
+    }
+
     public class Checkout
     {
         public double GetTotal()
         {
             return 0;
+        }
+
+        public void Scan(char item)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
