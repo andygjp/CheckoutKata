8c951ae065315bdc301361ae9078f5df2b46dcc9
Andrew Pattison <andygjp@hotmail.com>
2015-11-19 22:19:38 +0000

    Consolidated the types. I think two separate classes, as I had before, might be better.

--- CheckoutKata/CheckoutKata/Item.cs
+++ CheckoutKata/CheckoutKata/Item.cs
@@ -2,22 +2,46 @@ namespace CheckoutKata
 {
     public class Item
     {
-        public Item(int price)
+        private readonly int _price;
+        private int _scanned;
+        private readonly int _x;
+        private readonly int _promotion;
+
+        public static Item NonPromotional(int price)
         {
-            Price = price;
+            return new Item(price, 1, price);
         }
 
-        protected int Price { get; }
-        protected int Scanned { get; private set; }
-
-        public virtual int SubTotal()
+        public static Item BuyXForY(int price, int x, int promotion)
         {
-            return Price*Scanned;
+            return new Item(price, x, promotion);
+        }
+
+        private Item(int price, int x, int promotion)
+        {
+            _price = price;
+            _x = x;
+            _promotion = promotion;
+        }
+
+        public int SubTotal()
+        {
+            return GetPromotionalPrice() + GetNonPromotionalPrice();
+        }
+
+        private int GetNonPromotionalPrice()
+        {
+            return _scanned%_x*_price;
+        }
+
+        private int GetPromotionalPrice()
+        {
+            return _scanned/_x*_promotion;
         }
 
         public void Scan()
         {
-            Scanned++;
+            _scanned++;
         }
     }
 }
