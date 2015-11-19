539d71cf9ab156f7b044c416fb5acaedddaef151
Andrew Pattison <andygjp@hotmail.com>
2015-11-19 09:49:24 +0000

    Refactoring.

--- CheckoutKata/CheckoutKata.Tests/Tests.cs
+++ CheckoutKata/CheckoutKata.Tests/Tests.cs
@@ -33,7 +33,7 @@
     {
         // Return a new instance
         public Checkout Sut =>
-                new Checkout(new ItemPrice('A', 50.0, new GetXForY(3, 130.0)), new ItemPrice('B', 30, new GetXForY(2, 45.0)), new NormalPriceProxy('C', 20.0), new NormalPriceProxy('D', 15.0));
+                new Checkout(new GetXForYProxy('A', 50.0, new GetXForY(3, 130.0)), new GetXForYProxy('B', 30, new GetXForY(2, 45.0)), new NormalPriceProxy('C', 20.0), new NormalPriceProxy('D', 15.0));
     }
 
     public class When_multiple_items_are_scanned : IClassFixture<DefaultItemPricingFixture>
